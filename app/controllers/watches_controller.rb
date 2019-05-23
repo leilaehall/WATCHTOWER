a2673c727aa4f170dfc5d8e22b896d74152b48f1
Chris Hordijk <hordijk.chris@gmail.com>
2019-05-23 16:11:24 +0200

    search completed

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -2,8 +2,22 @@ class WatchesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @watches = Watch.all
-  end
+    if params[:query].present?
+
+      conditions = <<~SQL
+        watch_brand ILIKE :query OR
+        watch_model ILIKE :query OR
+        watch_category ILIKE :query OR
+        gender ILIKE :query
+        SQL
+
+      @watches = Watch.where(conditions, query: "%#{params[:query]}%")
+
+    else
+      @watches = Watch.all
+      end
+    end
+
 
   def show
     @watch = Watch.find(params[:id])
--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,7 +1,23 @@
 class Watch < ApplicationRecord
+  include PgSearch
   belongs_to :user
   has_many :bookings
   mount_uploader :watch_image, PhotoUploader
+
+
+ multisearchable against: [ :watch_brand, :watch_model, :watch_category, :rental_price, :retail_price, :gender, :user_id ]
+
+private
+
+  def search_data
+    {
+      watch_brand: watch_brand,
+      watch_model: watch_model,
+      watch_category: watch_category,
+      rental_price: rental_price,
+      retail_price: retail_price,
+      gender: gender,
+      user_id: user_id,
+    }
+  end
 end
-
-
